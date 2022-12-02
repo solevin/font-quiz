0944f5b6b6760d151977bccb3575b621c58ed38f
naru380 <naruhey1211guitar.m@gmail.com>
2022-12-02 09:01:15 +0900

    #40: change a logic to check full coverage

---
+++ test/coverage_test.dart
@@ -0,0 +1,34 @@
+/// *** GENERATED FILE - ANY CHANGES WOULD BE OBSOLETE ON NEXT GENERATION *** ///
+
+// ignore_for_file: unused_import
+
+/// Helper to test coverage for all project files
+import 'package:font_quiz/configs.dart';
+import 'package:font_quiz/constants.dart';
+import 'package:font_quiz/db/db_provider.dart';
+import 'package:font_quiz/db/font.dart';
+import 'package:font_quiz/db/font_dao.dart';
+import 'package:font_quiz/db/highscore.dart';
+import 'package:font_quiz/db/highscore_dao.dart';
+import 'package:font_quiz/db/question.dart';
+import 'package:font_quiz/db/question_dao.dart';
+import 'package:font_quiz/gen/assets.gen.dart';
+import 'package:font_quiz/main.dart';
+import 'package:font_quiz/router.dart';
+import 'package:font_quiz/settings.dart';
+// import 'package:font_quiz/settings.freezed.dart';
+import 'package:font_quiz/setttings_provider.dart';
+import 'package:font_quiz/ui/common/app_title_bar.dart';
+import 'package:font_quiz/ui/common/sound_view.dart';
+import 'package:font_quiz/ui/highscore/highscore_page.dart';
+import 'package:font_quiz/ui/highscore/highscore_page_view.dart';
+import 'package:font_quiz/ui/home/home_page.dart';
+import 'package:font_quiz/ui/play/play_page.dart';
+import 'package:font_quiz/ui/play/play_page_reverse.dart';
+import 'package:font_quiz/ui/play/play_setting_page.dart';
+import 'package:font_quiz/ui/play/play_setting_view.dart';
+import 'package:font_quiz/ui/play/result_page.dart';
+import 'package:font_quiz/ui/play/review_page.dart';
+import 'package:font_quiz/ui/view/view_page.dart';
+
+void main() {}
